e88fc5580337b5c9f39f257e90b7b1b2b275ae6c
Jaime Liew <jyli@dtu.dk>
2020-01-02 13:51:50 +0100

    Implement Moviepy in place of of FFMPEG

--- mandlebrot.py
+++ mandlebrot.py
@@ -2,55 +2,94 @@
 """
 Created on Wed Nov  7 19:37:33 2018
 
-@author: J
+@author: Jaime Liew
 """
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from contextlib import contextmanager
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
 
-interp = 'bilinear'
-#X, Y, R = -0.925, -0.266, 0.032
-#X, Y, R = -0.16, 1.0405, 0.026
-#X, Y, R = -0.745428, 0.113009, 3e-5
-#X, Y, R = -0.748, 0.1, 0.0014
-'''
--1.25066 0.02012 1.7e-4
 
-'''
+def mandelbrot(X, Y, R, nx):
+    '''
+    Yields the Mandelbrot set at each iteration at a given location.
 
-def mandelbrot(X, Y, R, nx, N_max):
+    args:
+        X (float): X (real) coordinate of interest.
+        Y (float): Y (imaginary) coordinate of interest.
+        R (float): Frame 'radius'.
+        nx (int): X and Y discretisation.
+    Yields:
+        Mand (nx x nx array): Mandelbrot set for the next iteration.
+    '''
+
     x = np.linspace(X - R, X + R, nx)
     y = np.linspace(Y - R, Y + R, nx)
-    c = x[:,np.newaxis] + 1j*y[np.newaxis,:]
+    c = x[:, np.newaxis] + 1j * y[np.newaxis, :]
     z = c
-    for j in range(N_max):
+    while True:
         z = z**2 + c
-        yield abs(z) < 2
+        mand = abs(z) < 2
+
+        # Ignore initial 'blank' iterations.
+        if all(x for x in mand.ravel()):
+            continue
+        else:
+            yield mand
 
 
-@contextmanager
-def make_image(idx):
-    plt.figure(figsize=[7,7])
+class Mandelbrot_image(object):
+    '''
+     Class that creates an animated gif of the mandelbrot set at a given
+     location.
+    '''
+    def __init__(self, X, Y, R, T_max, figsize=4, nx=500):
+        '''
+        args:
+            X (float): X (real) coordinate of interest.
+            Y (float): Y (imaginary) coordinate of interest.
+            R (float): Frame 'radius'.
+            T_max (float): GIF length in seconds.
+            figsize (float): figure side length (default=4).
+            nx (int): X and Y discretisation (default=500).
+        '''
+        self.mandelbrot_gen = mandelbrot(X, Y, R, nx=nx)
+        self.T_max = T_max
+        self.figsize = figsize
+        self.image = np.zeros([nx, nx])
 
-    yield plt.gca()
-    plt.axis('off')
-    plt.savefig('_temp/mandelbrot{:03d}.png'.format(idx), dpi=200)
-    plt.close()
+    def iterate(self, t):
+        '''
+            Performs one iteration of the Mandelbrot calculation and returns the
+            figure plotting the cumulative iterations.
+        '''
+        mand = next(self.mandelbrot_gen)
+        self.image += np.array(mand, dtype=float) * t / self.T_max
 
+        fig = plt.figure(figsize=[self.figsize, self.figsize])
+        plt.imshow(self.image.T,
+                   cmap='nipy_spectral_r',
+                   interpolation='bilinear')
+        plt.axis('off')
+        return fig
+
+    def make_gif(self, filename):
+        def make_frame(t):
+            fig = self.iterate(t)
+            return mplfig_to_npimage(fig)
+
+        animation = VideoClip(make_frame, duration=self.T_max)
+        animation.write_gif(filename, fps=20)
 
 
 if __name__ == '__main__':
+    params = { # filename: (X, Y, R, T_max)
+        'out1.gif': (-0.235125, 0.827215, 4.0E-5, 10),
+        'out2.gif': (-0.925, -0.266, 0.032, 10),
+        'out3.gif': (-0.745428, 0.113009, 3e-5, 10),
+        'out4.gif': (-0.748, 0.1, 0.0014, 20),
+    }
 
-    X, Y, R, nx, N_max = [float(arg) for arg in sys.argv[1:]]
-    nx, N_max = int(nx), int(N_max)
-    image = np.zeros([nx, nx])
-    idx = 0
-    for j, mand in enumerate(mandelbrot(X, Y, R, nx, N_max)):
-        image += np.array(mand, dtype=float)*j/N_max
-        if all(x for x in mand.ravel()):
-            continue
-        with make_image(idx) as ax:
-            ax.imshow(image.T, cmap='nipy_spectral_r', interpolation=interp)
-        print(f'\r{j}/{N_max}', end='')
-        idx += 1
+    for filename, (X, Y, R, T_max) in params.items():
+        mandelbrot_gif = Mandelbrot_image(X, Y, R, T_max)
+        mandelbrot_gif.make_gif(filename)
